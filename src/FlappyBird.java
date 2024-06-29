5ca202cf519a016751c5c93a543fe998e1b0d790
SaolGhra <DeathCRystalYT@Gmail.com>
2024-06-29 04:21:54 +0100

    V1.1.0
    The bird can fall now

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -5,9 +5,11 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
+import javax.swing.Timer;
+
 import javax.swing.*;
 
-public class FlappyBird extends JPanel {
+public class FlappyBird extends JPanel implements ActionListener {
     int boardWidth = 360;
     int boardHeight = 640;
 
@@ -38,6 +40,11 @@ public class FlappyBird extends JPanel {
     // Game logic
     Bird bird;
 
+    int velocityY = -6;
+    int gravity = 1;
+
+    Timer gameLoop;
+
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.cyan);
@@ -50,18 +57,50 @@ public class FlappyBird extends JPanel {
 
         // Bird
         bird = new Bird(birdImg);
+
+        // Game loop timer
+        gameLoop = new Timer(1000 / 60, this); // 60 FPS (1 second / 60) = 16.6666666667 milliseconds per frame (rounded
+                                               // to 17)
+        gameLoop.start(); // Start the game loop
     }
 
+    // Paint the game
     public void paintComponent(Graphics g) {
+        // Call the super class
         super.paintComponent(g);
+
+        // Draw the game objects using the draw method
         draw(g);
     }
 
+    // Draw the game objects on the screen
     public void draw(Graphics g) {
+        // System.out.println("Drawing");
+
         // Draw the background
         g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
 
         // Draw the bird
         g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+    }
+
+    public void move() {
+        // Apply gravity to the bird's velocity
+        velocityY += gravity;
+
+        // Move the bird's Y position
+        bird.y += velocityY;
+
+        // Adding a ceiling to the bird
+        bird.y = Math.max(bird.y, 0);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Update the position of the bird 60 times per second
+        move();
+
+        // Move the bird
+        repaint();
     }
 }
